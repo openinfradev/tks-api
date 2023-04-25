c2e368f7afe09a298497ef83158404b20449a4c5
Seungkyu Ahn <seungkyua@gmail.com>
2023-04-25 15:14:51 +0900

    chang camel case

--- pkg/domain/app-serve-app.go
+++ pkg/domain/app-serve-app.go
@@ -27,17 +27,17 @@ type AppServeApp struct {
 	TargetClusterId string `json:"targetClusterId,omitempty"`
 	// status is status of deployed app
 	Status string `json:"status,omitempty"`
-	// created_at is a creatioin timestamp for the application
+	// created_at is a creation timestamp for the application
 	CreatedAt        time.Time         `gorm:"autoCreateTime:false" json:"createdAt" `
 	UpdatedAt        *time.Time        `gorm:"autoUpdateTime:false" json:"updatedAt"`
 	DeletedAt        *time.Time        `json:"deleted_at"`
-	AppServeAppTasks []AppServeAppTask `gorm:"foreignKey:AppServeAppId" json:"app_serve_app_tasks"`
+	AppServeAppTasks []AppServeAppTask `gorm:"foreignKey:AppServeAppId" json:"appServeAppTasks"`
 }
 
 type AppServeAppTask struct {
 	ID string `gorm:"primarykey" json:"id,omitempty"`
 	// ID for appServeApp that this task belongs to.
-	AppServeAppId string `gorm:"not null" json:"app_serve_app_id,omitempty"`
+	AppServeAppId string `gorm:"not null" json:"appServeAppId,omitempty"`
 	// application version
 	Version string `json:"version,omitempty"`
 	// status is app status
@@ -45,36 +45,36 @@ type AppServeAppTask struct {
 	// output for task result
 	Output string `json:"output,omitempty"`
 	// URL of java app artifact (Eg, Jar)
-	ArtifactUrl string `json:"artifact_url,omitempty"`
+	ArtifactUrl string `json:"artifactUrl,omitempty"`
 	// URL of built image for app
-	ImageUrl string `json:"image_url,omitempty"`
+	ImageUrl string `json:"imageUrl,omitempty"`
 	// Executable path of app image
-	ExecutablePath string `json:"executable_path,omitempty"`
+	ExecutablePath string `json:"executablePath,omitempty"`
 	// java app profile
 	Profile string `json:"profile,omitempty"`
 	// java app config
-	AppConfig string `json:"app_config,omitempty"`
+	AppConfig string `json:"appConfig,omitempty"`
 	// java app secret
-	AppSecret string `json:"app_secret,omitempty"`
+	AppSecret string `json:"appSecret,omitempty"`
 	// env variable list for java app
-	ExtraEnv string `json:"extra_env,omitempty"`
+	ExtraEnv string `json:"extraEnv,omitempty"`
 	// java app port
 	Port string `json:"port,omitempty"`
 	// resource spec of app pod
-	ResourceSpec string `json:"resource_spec,omitempty"`
+	ResourceSpec string `json:"resourceSpec,omitempty"`
 	// revision of deployed helm release
-	HelmRevision int32 `json:"helm_revision,omitempty"`
+	HelmRevision int32 `json:"helmRevision,omitempty"`
 	// deployment strategy (eg, rolling-update)
 	Strategy       string `json:"strategy,omitempty"`
-	PvEnabled      bool   `json:"pv_enabled"`
-	PvStorageClass string `json:"pv_storage_class"`
-	PvAccessMode   string `json:"pv_access_mode"`
-	PvSize         string `json:"pv_size"`
-	PvMountPath    string `json:"pv_mount_path"`
+	PvEnabled      bool   `json:"pvEnabled"`
+	PvStorageClass string `json:"pvStorageClass"`
+	PvAccessMode   string `json:"pvAccessMode"`
+	PvSize         string `json:"pvSize"`
+	PvMountPath    string `json:"pvMountPath"`
 	// created_at is  a creation timestamp for the application
-	CreatedAt time.Time  `gorm:"autoCreateTime:false" json:"created_at"`
-	UpdatedAt *time.Time `gorm:"autoUpdateTime:false" json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at"`
+	CreatedAt time.Time  `gorm:"autoCreateTime:false" json:"createdAt"`
+	UpdatedAt *time.Time `gorm:"autoUpdateTime:false" json:"updatedAt"`
+	DeletedAt *time.Time `json:"deletedAt"`
 }
 
 func (a *AppServeApp) BeforeCreate(tx *gorm.DB) (err error) {
@@ -91,27 +91,27 @@ type CreateAppServeAppRequest struct {
 	// App
 	Name            string `json:"name" validate:"required"`
 	Namespace       string `json:"namespace"`
-	Type            string `json:"type" `    // build deploy all
-	AppType         string `json:"app_type"` // springboot spring
-	TargetClusterId string `json:"target_cluster_id" validate:"required"`
+	Type            string `json:"type" `   // build deploy all
+	AppType         string `json:"appType"` // springboot spring
+	TargetClusterId string `json:"targetClusterId" validate:"required"`
 
 	// Task
 	Version        string `json:"version"`
 	Strategy       string `json:"strategy"` // rolling-update blue-green canary
-	ArtifactUrl    string `json:"artifact_url"`
-	ImageUrl       string `json:"image_url"`
-	ExecutablePath string `json:"executable_path"`
-	ResourceSpec   string `json:"resource_spec"` // tiny medium large
+	ArtifactUrl    string `json:"artifactUrl"`
+	ImageUrl       string `json:"imageUrl"`
+	ExecutablePath string `json:"executablePath"`
+	ResourceSpec   string `json:"resourceSpec"` // tiny medium large
 	Profile        string `json:"profile"`
-	AppConfig      string `json:"app_config"`
-	AppSecret      string `json:"app_secret"`
-	ExtraEnv       string `json:"extra_env"`
+	AppConfig      string `json:"appConfig"`
+	AppSecret      string `json:"appSecret"`
+	ExtraEnv       string `json:"extraEnv"`
 	Port           string `json:"port"`
-	PvEnabled      bool   `json:"pv_enabled"`
-	PvStorageClass string `json:"pv_storage_class"`
-	PvAccessMode   string `json:"pv_access_mode"`
-	PvSize         string `json:"pv_size"`
-	PvMountPath    string `json:"pv_mount_path"`
+	PvEnabled      bool   `json:"pvEnabled"`
+	PvStorageClass string `json:"pvStorageClass"`
+	PvAccessMode   string `json:"pvAccessMode"`
+	PvSize         string `json:"pvSize"`
+	PvMountPath    string `json:"pvMountPath"`
 }
 
 func (c *CreateAppServeAppRequest) SetDefaultValue() {
@@ -142,38 +142,38 @@ func (c *CreateAppServeAppRequest) SetDefaultValue() {
 }
 
 type CreateAppServeAppResponse struct {
-	ID   string `json:"app_id"`
-	Name string `json:"app_name"`
+	ID   string `json:"appId"`
+	Name string `json:"appName"`
 }
 
 type UpdateAppServeAppStatusRequest struct {
-	TaskID string `json:"task_id" validate:"required"`
+	TaskID string `json:"taskId" validate:"required"`
 	Status string `json:"status" validate:"required"`
 	Output string `json:"output"`
 }
 
 type UpdateAppServeAppEndpointRequest struct {
-	TaskID             string `json:"task_id" validate:"required"`
-	EndpointUrl        string `json:"endpoint_url"`
-	PreviewEndpointUrl string `json:"preview_endpoint_url"`
-	HelmRevision       int32  `json:"helm_revision"`
+	TaskID             string `json:"taskId" validate:"required"`
+	EndpointUrl        string `json:"endpointUrl"`
+	PreviewEndpointUrl string `json:"previewEndpointUrl"`
+	HelmRevision       int32  `json:"helmRevision"`
 }
 
 type UpdateAppServeAppRequest struct {
 	// App
 	Type    string `json:"type"`
-	AppType string `json:"app_type"`
+	AppType string `json:"appType"`
 
 	// Task
 	Strategy       string `json:"strategy"`
-	ArtifactUrl    string `json:"artifact_url"`
-	ImageUrl       string `json:"image_url"`
-	ExecutablePath string `json:"executable_path"`
-	ResourceSpec   string `json:"resource_spec"`
+	ArtifactUrl    string `json:"artifactUrl"`
+	ImageUrl       string `json:"imageUrl"`
+	ExecutablePath string `json:"executablePath"`
+	ResourceSpec   string `json:"resourceSpec"`
 	Profile        string `json:"profile"`
-	AppConfig      string `json:"app_config"`
-	AppSecret      string `json:"app_secret"`
-	ExtraEnv       string `json:"extra_env"`
+	AppConfig      string `json:"appConfig"`
+	AppSecret      string `json:"appSecret"`
+	ExtraEnv       string `json:"extraEnv"`
 	Port           string `json:"port"`
 
 	// Update Strategy
@@ -203,9 +203,9 @@ func (u *UpdateAppServeAppRequest) SetDefaultValue() {
 }
 
 type GetAppServeAppsResponse struct {
-	AppServeApps []AppServeApp `json:"app_serve_apps"`
+	AppServeApps []AppServeApp `json:"appServeApps"`
 }
 
 type GetAppServeAppResponse struct {
-	AppServeApp AppServeApp `json:"app_serve_app"`
+	AppServeApp AppServeApp `json:"appServeApp"`
 }
