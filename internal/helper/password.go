ec3bbcf016c16e0c77f8cab20dcd01a9ae363c8c
donggyu <cho4036@gmail.com>
2023-05-04 13:50:55 +0900

    improve: improve generating temporary password logic with crypto package

--- internal/helper/password.go
+++ internal/helper/password.go
@@ -1,14 +1,10 @@
 package helper
 
 import (
+	"crypto/rand"
 	"golang.org/x/crypto/bcrypt"
-	"math/rand"
-	"time"
+	"math/big"
 )
-
-func init() {
-	rand.NewSource(time.Now().UnixNano())
-}
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -28,8 +24,15 @@ func GenerateRandomString(length int) string {
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	b := make([]rune, length)
 	for i := range b {
-		num := rand.Intn(len(letters))
-		b[i] = letters[num]
+		b[i] = randomRune(letters)
 	}
 	return string(b)
+}
+
+func randomRune(chars []rune) rune {
+	n, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+	if err != nil {
+		panic(err)
+	}
+	return chars[n.Int64()]
 }
--- internal/helper/password_test.go
+++ internal/helper/password_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestPasswordGeneration(t *testing.T) {
+func TestPasswordHashing(t *testing.T) {
 	password := "admin"
 	hashedPassword, err := helper.HashPassword(password)
 	fmt.Println(hashedPassword)
@@ -18,4 +18,19 @@ func TestPasswordGeneration(t *testing.T) {
 		t.Errorf("CheckPasswordHash() error")
 		return
 	}
+}
+
+func TestPasswordGeneration(t *testing.T) {
+	length := 8
+	sameCount := 0
+	for i := 0; i < 10000000; i++ {
+		firstGenPassword := helper.GenerateRandomString(length)
+		secondGenPassword := helper.GenerateRandomString(length)
+		if firstGenPassword == secondGenPassword {
+			fmt.Printf("Index: %d. It's same %s\n", i, firstGenPassword)
+			sameCount++
+		}
+	}
+	fmt.Println("Same count: ", sameCount)
+	fmt.Println("Same ratio: ", float64(sameCount)/100000)
 }
