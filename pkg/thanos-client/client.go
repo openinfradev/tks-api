460d3a605b012705209bc97e5670dd3469478701
Taekyu <ktkfree@naver.com>
2023-05-10 23:45:14 +0900

    feature. imple promql for dashboard

--- pkg/thanos-client/client.go
+++ pkg/thanos-client/client.go
@@ -14,6 +14,7 @@ import (
 
 type ThanosClient interface {
 	Get(query string) (Metric, error)
+	FetchRange(query string, start int, end int, step int) (out Metric, err error)
 }
 
 type ThanosClientImpl struct {
@@ -73,5 +74,41 @@ func (c *ThanosClientImpl) Get(query string) (out Metric, err error) {
 	}
 
 	log.Info(helper.ModelToJson(out))
+	return
+}
+
+func (c *ThanosClientImpl) FetchRange(query string, start int, end int, step int) (out Metric, err error) {
+	rangeParam := fmt.Sprintf("&dedup=true&partial_response=false&start=%d&end=%d&step=%d&max_source_resolution=0s", start, end, step)
+	query = url.QueryEscape(query) + rangeParam
+	url := c.url + "/api/v1/query_range?query=" + query
+	res, err := c.client.Get(url)
+	if err != nil {
+		return out, err
+	}
+	if res == nil {
+		return out, fmt.Errorf("Failed to call thanos.")
+	}
+	if res.StatusCode != 200 {
+		log.Info(res)
+		return out, fmt.Errorf("Invalid http status. return code: %d", res.StatusCode)
+	}
+
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			log.Error("error closing http body")
+		}
+	}()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return out, err
+	}
+
+	err = json.Unmarshal(body, &out)
+	if err != nil {
+		return out, err
+	}
+
+	//log.Info(helper.ModelToJson(out))
 	return
 }
