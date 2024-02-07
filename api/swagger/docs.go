b956bb63cc37853b67509369d42cc19204ba1151
taekyu.kang <ktkfree@naver.com>
2024-02-07 11:20:47 +0900

    change swagger url to dev

--- api/swagger/docs.go
+++ api/swagger/docs.go
@@ -8166,7 +8166,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "tks-api-ft.taco-cat.xyz",
+	Host:             "tks-api-dev.taco-cat.xyz",
 	BasePath:         "/api/1.0/",
 	Schemes:          []string{},
 	Title:            "tks-api service",
