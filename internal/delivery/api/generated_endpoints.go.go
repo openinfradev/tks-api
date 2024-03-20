081bcef6f9d516bd5c498ef3eb47ec3a1e7c56f0
Seungkyu Ahn <seungkyua@gmail.com>
2024-03-20 13:31:39 +0900

    dashboard generated_endpoints

--- internal/delivery/api/generated_endpoints.go.go
+++ internal/delivery/api/generated_endpoints.go.go
@@ -319,6 +319,18 @@ var ApiMap = map[Endpoint]EndpointInfo{
 		Name: "GetOrganizationStackTemplate", 
 		Group: "StackTemplate",
 	},
+    CreateDashboard: {
+		Name: "CreateDashboard", 
+		Group: "Dashboard",
+	},
+    GetDashboard: {
+		Name: "GetDashboard", 
+		Group: "Dashboard",
+	},
+    UpdateDashboard: {
+		Name: "UpdateDashboard", 
+		Group: "Dashboard",
+	},
     GetChartsDashboard: {
 		Name: "GetChartsDashboard", 
 		Group: "Dashboard",
@@ -940,6 +952,12 @@ func (e Endpoint) String() string {
 		return "GetOrganizationStackTemplates"
 	case GetOrganizationStackTemplate:
 		return "GetOrganizationStackTemplate"
+	case CreateDashboard:
+		return "CreateDashboard"
+	case GetDashboard:
+		return "GetDashboard"
+	case UpdateDashboard:
+		return "UpdateDashboard"
 	case GetChartsDashboard:
 		return "GetChartsDashboard"
 	case GetChartDashboard:
@@ -1334,6 +1352,12 @@ func GetEndpoint(name string) Endpoint {
 		return GetOrganizationStackTemplates
 	case "GetOrganizationStackTemplate":
 		return GetOrganizationStackTemplate
+	case "CreateDashboard":
+		return CreateDashboard
+	case "GetDashboard":
+		return GetDashboard
+	case "UpdateDashboard":
+		return UpdateDashboard
 	case "GetChartsDashboard":
 		return GetChartsDashboard
 	case "GetChartDashboard":
